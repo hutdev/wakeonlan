e7aaef94589f0e93c22ad72097cf4d92ec4a140a
hut <hutdevelopment@gmail.com>
2014-05-17 17:52:07 +0200

    DB support

--- index.js
+++ index.js
@@ -1,37 +1,101 @@
 "use strict";
 
 var clipp = require('clipp'),
+    cli = clipp.parse(['save','s']),
     client = require('dgram').createSocket('udp4'),
     logger = require('./logging'),
-    address = require('clipp').get(['a','address']), 
-    packet = 'FFFFFFFFFFFF';
+    address = clipp.get(['a','address'],cli), 
+    machine = clipp.get(['m','machine'],cli), 
+    save = clipp.get(['s','save'],cli),
+    packet = 'FFFFFFFFFFFF',
+    db = require('./db');
+
+function exit(signal){
+  db.close();
+  process.exit(signal);
+}
+
+function prepareClient(){
+    address = address.replace(/[-:]/g,'');
+    for (var idx = 0; idx < 16; idx++)
+      packet = packet + address;
+
+    client.bind(0, '0.0.0.0', broadcastPacket);
+}
 
 function sendcallback(err){
   client.close();
   if (err){
     logger.error('Could not send WOL  packet', err);
-    process.exit(3);
+    exit(3);
   }
   logger.info('WOL packet successfully sent');
+  exit(0);
 }
 
 function broadcastPacket(err){
   var msgbuf = new Buffer(packet, 'hex');
   if (err){
     logger.error('Could not initialize client socket', err);
-    process.exit(2);
+    exit(2);
   }
   client.setBroadcast(true);
   client.send(msgbuf, 0, msgbuf.length, 9, '255.255.255.255', sendcallback); 
 }
 
-if (!address || !address.match(/^([A-Za-z\d]{2}[-:]){5}[A-Za-z\d]{2}$/)){
- logger.error('Please specify MAC address as parameter');
- process.exit(1);
+function checkAddress(exit){
+  if (!address || !address.match(/^([A-Za-z\d]{2}[-:]){5}[A-Za-z\d]{2}$/)){
+    if(exit){
+       logger.error('Please specify MAC address as parameter');
+       exit(1);
+    }
+    return false;
+  }
+  return true;
 }
-address = address.replace(/[-:]/g,'');
 
-for (var idx = 0; idx < 16; idx++)
-  packet = packet + address;
+function checkMachine(){
+  if (!machine){
+   logger.error('Please specify machine as parameter');
+   exit(1);
+  }
+}
 
-client.bind(0, '0.0.0.0', broadcastPacket);
+if (save){
+  checkAddress(true);
+  checkMachine();
+  db.address(machine,address,function(err){
+    if(err){
+      logger.error('Could not save address',err);
+      exit(4);
+    }
+    else{
+      logger.info('Saving successful');
+      exit(0);
+    }
+  });
+}
+else{
+  if(checkAddress()){
+    prepareClient();
+  }
+  else {
+    checkMachine();
+    db.address(machine,function(err,mac){
+      if (!err)
+        if (mac){
+          address = mac;
+          prepareClient();
+        }
+        else{
+          logger.error('MAC not found for ' + machine);
+          exit(6);
+        }
+      else{
+        logger.error('Could not load mac for ' + machine,ex);
+        exit(5);
+      }
+    });
+  }
+}
+
