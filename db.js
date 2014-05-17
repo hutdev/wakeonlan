77c66cfa0926763c54c0959b2e00f40f15cf379c
hut <hutdevelopment@gmail.com>
2014-05-17 17:15:35 +0200

    Initial import

---
+++ db.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var sqlite = require('sqlite3'),
+    logger = require('./logging'),
+    db;
+
+function openDatabase(callback){
+  if(!db){
+    db = new sqlite.Database('machines.db',function(oerr){
+      if (oerr){
+        logger.error("Could not open database ", oerr);
+        callback(oerr);
+      }
+      db.exec(
+        'CREATE TABLE IF NOT EXISTS address (mac CHARACTER(17), machine VARCHAR(50) PRIMARY KEY)',
+        callback);
+    });
+  }
+  else
+    callback();
+}
+
+exports.close = function(callback){
+/**  if (db)
+    db.close(callback);*/
+};
+
+exports.address = function(name,a,b){
+  var callback = typeof(a) === 'function' ? a : b;
+  openDatabase(function(err){
+    if (typeof(a) === 'function'){
+      logger.info('Selecting mac for ' + name);
+      db.get('SELECT mac FROM address WHERE machine = ?',[name],
+        function(error,row){
+          callback(error,row ? row.mac : row);
+        });
+    }
+    else {
+      logger.info('Saving mac ' + a + ' for ' + name);
+      db.exec('REPLACE INTO address(mac,machine) VALUES ("' + a + '","' + name + '")',
+        callback);
+    }
+  });
+};
+
