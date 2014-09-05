85ab2062f10a52d783edc7f98994cf3a73c7a30a
hut <hutdevelopment@gmail.com>
2014-09-05 23:33:04 +0200

    Formatting

--- db.js
+++ db.js
@@ -3,54 +3,54 @@
 var sqlite = require('sqlite3'),
     logger = require('./logging'),
     path = require('path'),
-    dbpath = path.resolve(path.dirname(require('clipp').parse().script),'./machines.db'),
+    dbpath = path.resolve(path.dirname(require('clipp').parse().script), './machines.db'),
     db;
 
-function openDatabase(callback){
-  if(!db){
-    db = new sqlite.Database(dbpath,function(oerr){
-      if (oerr){
-        logger.error("Could not open database ", oerr);
-        callback(oerr);
-      }
-      db.exec(
-        'CREATE TABLE IF NOT EXISTS address (mac CHARACTER(17), machine VARCHAR(50) PRIMARY KEY)',
-        callback);
-    });
-  }
-  else
-    callback();
-}
-
-exports.close = function(callback){
-  if (db)
-    db.close(callback);
-};
-
-exports.address = function(name,a,b){
-  var callback = typeof(a) === 'function' ? a : b;
-  openDatabase(function(err){
-    if (typeof(a) === 'function'){
-      logger.info('Selecting mac for ' + name);
-      db.get('SELECT mac FROM address WHERE machine = ?',[name],
-        function(error,row){
-          callback(error,row ? row.mac : row);
+function openDatabase(callback) {
+    if (!db) {
+        db = new sqlite.Database(dbpath, function (oerr) {
+            if (oerr) {
+                logger.error("Could not open database ", oerr);
+                callback(oerr);
+            }
+            db.exec(
+                'CREATE TABLE IF NOT EXISTS address (mac CHARACTER(17), machine VARCHAR(50) PRIMARY KEY)',
+                callback);
         });
     }
-    else {
-      logger.info('Saving mac ' + a + ' for ' + name);
-      db.run('REPLACE INTO address(mac,machine) VALUES (?,?)',
-        a,name,
-        callback);
-    }
-  });
+    else
+        callback();
+}
+
+exports.close = function (callback) {
+    if (db)
+        db.close(callback);
 };
 
-exports.list = function(callback){
-  openDatabase(function(err){
-    if (err)
-      callback(err);
-    else
-      db.all('SELECT * FROM address',callback);
-  });
+exports.address = function (name, a, b) {
+    var callback = typeof(a) === 'function' ? a : b;
+    openDatabase(function (err) {
+        if (typeof(a) === 'function') {
+            logger.info('Selecting mac for ' + name);
+            db.get('SELECT mac FROM address WHERE machine = ?', [name],
+                function (error, row) {
+                    callback(error, row ? row.mac : row);
+                });
+        }
+        else {
+            logger.info('Saving mac ' + a + ' for ' + name);
+            db.run('REPLACE INTO address(mac,machine) VALUES (?,?)',
+                a, name,
+                callback);
+        }
+    });
+};
+
+exports.list = function (callback) {
+    openDatabase(function (err) {
+        if (err)
+            callback(err);
+        else
+            db.all('SELECT * FROM address', callback);
+    });
 };
--- logging.js
+++ logging.js
@@ -2,15 +2,15 @@
 
 var winston = require('winston'),
     logger = new (winston.Logger)({
-      transports: [
-        new (winston.transports.Console)(),
-        new (winston.transports.File)({ 
-          filename: 'wakeonlan-error.log', 
-          json: false, 
-          level: 'error' ,
-          handleExceptions: true
-        })
-      ]
+        transports: [
+            new (winston.transports.Console)(),
+            new (winston.transports.File)({
+                filename: 'wakeonlan-error.log',
+                json: false,
+                level: 'error',
+                handleExceptions: true
+            })
+        ]
     });
 
 exports.info = logger.info;
